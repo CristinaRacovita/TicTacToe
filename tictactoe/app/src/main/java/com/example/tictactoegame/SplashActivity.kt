ba14d98ef6c0c84a19c607792b67d3cb4f9767ef
Cristina Racovita <cristina.racovita98@gmail.com>
2020-09-24 11:52:10 +0300

    splash activity

---
+++ tictactoe/app/src/main/java/com/example/tictactoegame/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.example.tictactoegame
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val splashTimeMilliseconds: Long = 2000
+        Handler().postDelayed({
+            val startIntent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(startIntent)
+            finish()
+        }, splashTimeMilliseconds)
+
+    }
+}
